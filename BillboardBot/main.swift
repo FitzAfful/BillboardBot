c3c657187f84ecfdb574cca8185a04aff7c93358
Fitzgerald Afful <fitzafful@gmail.com>
2018-11-18 11:18:09 +0000

    Initial Commit

---
+++ BillboardBot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BillboardBot
+//
+//  Created by Fitzgerald Afful on 18/11/2018.
+//  Copyright © 2018 Fitzgerald Afful. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
